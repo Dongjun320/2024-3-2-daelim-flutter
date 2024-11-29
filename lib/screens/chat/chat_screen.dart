30004ffcbd65a2c1a2ce8ac8fb6120fc239cb467
mrino <ehdwns010320@naver.com>
2024-11-29 13:25:39 +0900

    14주차 추가 수정

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,85 +1,129 @@
-import 'package:daelim_project/helpers/api_helper.dart';
 import 'package:easy_extension/easy_extension.dart';
 import 'package:flutter/material.dart';
-
 class ChatScreen extends StatefulWidget {
   final String roomId;
-  const ChatScreen({super.key, required this.roomId});
-
+  const ChatScreen({
+    super.key,
+    required this.roomId,
+  });
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
-
 class _ChatScreenState extends State<ChatScreen> {
   final _primaryColor = const Color(0xFF4E80EE);
   final _secondaryColor = Colors.white;
-  final _backgroundColor = const Color(0xFFF4F4F6);
-
-  final _dummyChatList = List.generate(6, (index) {
+  final _backgroundColor = const Color(0xFFF3F4F6);
+  var _dummyChatList = List<Map<String, dynamic>>.generate(6, (index) {
     return {
-      'sender_id': 'a',
-      'message': '안녕하세요! 눈이 너무 많이 오네요',
-      'create_at': DateTime.now().add(-index.toMinute),
+      'sender_id': index % 2 == 0 ? 'b' : 'a',
+      'message': '현재 메시지 테스트 중입니다.',
+      'created_at': DateTime.now().add(-index.toMinute),
     };
   });
-
+  @override
+  void initState() {
+    super.initState();
+    _dummyChatList = _dummyChatList.sortedBy((e) => e['created_at']);
+  }
   void _onSendMessage() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: _backgroundColor,
-        appBar: AppBar(
-          title: const Text('챗봇'),
-        ),
-        body: Column(
-          children: [
-            const Expanded(
-              child: SizedBox.shrink(),
+      backgroundColor: _backgroundColor,
+      appBar: AppBar(
+        title: const Text('챗봇'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              itemCount: _dummyChatList.length,
+              separatorBuilder: (context, index) {
+                return 10.heightBox;
+              },
+              itemBuilder: (context, index) {
+                final dummy = _dummyChatList[index];
+                final String senderId = dummy['sender_id'];
+                final String message = dummy['message'];
+                final DateTime createdAt = dummy['created_at'];
+                final isMy = senderId == 'a';
+                return Row(
+                  mainAxisAlignment: isMy //
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      constraints: const BoxConstraints(
+                        maxWidth: 250,
+                        minWidth: 50,
+                      ),
+                      color: isMy //
+                          ? _primaryColor
+                          : _secondaryColor,
+                      child: ListTile(
+                        title: Text(message),
+                        subtitle: Text(
+                          createdAt.toFormat('HH:mm'),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: _secondaryColor,
-                border: const Border(
-                  top: BorderSide(color: Colors.grey),
+          ),
+          // NOTE: 메시지 전송 영역
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: _secondaryColor,
+              border: Border(
+                top: BorderSide(
+                  color: Colors.grey[300]!,
                 ),
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: '메시지를 입력하세요....',
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                        ),
-                        filled: false,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: false,
+                      hintText: '메시지를 입력하세요...',
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
                       ),
                     ),
                   ),
-                  10.widthBox,
-                  ElevatedButton(
-                    onPressed: _onSendMessage,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: _primaryColor),
-                    child: const Text(
-                      '전송',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                      ),
+                ),
+                10.widthBox,
+                ElevatedButton(
+                  onPressed: _onSendMessage,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: _primaryColor,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 0,
                     ),
                   ),
-                ],
-              ),
-            )
-          ],
-        ));
+                  child: const Text(
+                    '전송',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
